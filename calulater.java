57cb5e8ba30a18a600f069d63c9e4c5b30370e33
Sumakubas <157406442+Sumakubas@users.noreply.github.com>
2024-02-19 15:18:51 +0530

    Update calulater.java

--- calulater.java
+++ calulater.java
@@ -14,9 +14,7 @@ public class calculate1 {
     public int div(int a, int b) {
         return a / b;
     }
-}
 
-class result {
     public static void main(String args[]) {
         calculate1 c = new calculate1();
         System.out.println("Addition=" + c.add(5, 3));
